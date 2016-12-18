fca069a9c7ff00548c41454ef0a0a615ca6eb3ea
Boris K <boris.koyfman@gmail.com>
2016-12-17 23:42:30 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+var main = function() {
+  $('.nav li').click(function() {
+    var category = $(this).attr('class');
+     $('.nav li').removeClass('active');
+    $(this).addClass('active');
+
+   if (category === 'nav-consumer') {
+     $('.thumbnail').removeClass('selected');
+     $('.consumer').addClass('selected');
+   } else if (category === 'nav-mobile') {
+     $('.thumbnail').removeClass('selected');
+     $('.mobile').addClass('selected');
+   } else if (category === 'nav-commerce') {
+     $('.thumbnail').removeClass('selected');
+     $('.commerce').addClass('selected');
+   } else if (category === 'nav-enterprise') {
+     $('.thumbnail').removeClass('selected');
+     $('.enterprise').addClass('selected');
+   } else if (category === 'nav-all') {
+     $('.thumbnail').removeClass('selected');
+   }
+        
+  });
+};
+ 
+$(document).ready(main);
+
+/*
+var main = function() {
+  $('.nav li').click(function() {
+    var category = $(this).attr('class');
+
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+    
+    if(category === "nav-consumer") {
+      $(".thumbnail").removeClass("selected");
+      $(".consumer").addClass("selected");
+    } else if(category === "nav-mobile") {
+      $(".thumbnail").removeClass("selected");
+      $(".mobile").addClass("selected");
+    } else if(category === "nav-commerce") {
+      $(".thumbnail").removeClass("selected");
+      $(".commerce").addClass("selected");
+    } else if(category === "nav-enterprise") {
+      $(".thumbnail").removeClass("selected");
+      $(".enterprise").addClass("selected");
+    } else {
+      $(".thumbnail").removeClass("selected");
+    }
+    
+  });
+};
+ 
+$(document).ready(main);
+
+http://codepen.io/krismis/pen/BKEQQv
+*/
